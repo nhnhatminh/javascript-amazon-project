8e94138da337bd9cf9fbd57e91348c5e62ad38f5
nhnhatminh <nhnm0609@gmail.com>
2026-03-07 21:42:41 +0700

    Save data and generate HTML for delivery options.

--- data/cart.js
+++ data/cart.js
@@ -6,14 +6,16 @@ if(!cart) {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       productName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
       quantity: 1,
+      deliveryOptionId: '1',
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       productName: "Intermediate Size Basketball",
       quantity: 1,
+      deliveryOptionId: '2',
     },
-  ];
-}
+  ]
+};
 
 function saveToLocalStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
@@ -36,6 +38,7 @@ export function addToCart(productId, productName) {
       productId: productId,
       productName: productName,
       quantity: 1,
+      deliveryOptionId: '1',
     });
   }
 
